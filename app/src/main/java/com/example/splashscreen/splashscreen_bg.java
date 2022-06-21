a2d75dcebd7769d0ca43e193a3d8b1de32409704
jleenaife <107950772+jleenaife@users.noreply.github.com>
2022-06-22 02:39:14 +0800

    Eco-Nice App

---
+++ app/src/main/java/com/example/splashscreen/splashscreen_bg.java
@@ -0,0 +1,14 @@
+package com.example.splashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class splashscreen_bg extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen_bg);
+    }
+}
