9a4b1c283548fefe895cf35ef7bb852d8b34c2a7
jleenaife <107950772+jleenaife@users.noreply.github.com>
2022-07-05 19:13:00 +0800

    quiz game (unfinished 2)

--- app/src/main/java/com/example/splashscreen/QuestionActivity.java
+++ app/src/main/java/com/example/splashscreen/QuestionActivity.java
@@ -3,17 +3,20 @@ package com.example.splashscreen;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class QuestionActivity extends AppCompatActivity implements View.OnClickListener{
 
     private TextView question, qCount, timer;
     private Button option1, option2;
     private List<Question> questionList;
+    int quesNum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,8 +53,84 @@ public class QuestionActivity extends AppCompatActivity implements View.OnClickL
         setQuestion();
     }
 
-    @Override
-    public void onClick(View view) {
+    public void setQuestion()
+    {
+        timer.setText(String.valueOf(10));
 
+        question.setText(questionList.get(0).getQuestion());
+        option1.setText(questionList.get(0).getOptionA());
+        option2.setText(questionList.get(0).getOptionB());
+
+        qCount.setText(String.valueOf(1) + "/" + String.valueOf(questionList.size()));
+
+        startTimer();
+
+        quesNum = 0;
+    }
+
+    private void startTimer()
+    {
+        CountDownTimer countDown = new CountDownTimer(1000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+                timer.setText(String.valueOf(millisUntilFinished/1000));
+            }
+
+            @Override
+            public void onFinish() {
+                changeQuestion();
+            }
+        };
+
+        countDown.start();
+    }
+
+
+
+    @Override
+    public void onClick(View v) {
+
+        int selectedOption = 0;
+
+        switch (v.getId())
+        {
+            case R.id.option1:
+                selectedOption = 1;
+                break;
+            case R.id.option2:
+                selectedOption = 2;
+                break;
+            default:
+        }
+
+        checkAnswer(selectedOption);
+    }
+
+    private void checkAnswer(int selectedOption)
+    {
+
+        if(selectedOption == questionList.get(quesNum).getCorrectAns())
+        {
+            //Right Answer
+        }
+        else
+        {
+            //Wrong Answer
+        }
+
+        changeQuestion();
+    }
+
+    private void changeQuestion()
+    {
+        if( quesNum < questionList.size() -1)
+        {
+
+        }
+        else
+        {
+
+        }
     }
 }
