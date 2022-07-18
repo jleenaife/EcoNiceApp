9f6f32e441514cbdb873e0aedee02c9bf6682267
avadecer <avadecer@gmail.com>
2022-07-18 17:03:48 +0800

    connectpg2(quiz game)

--- app/src/main/java/com/example/splashscreen/below12home.java
+++ app/src/main/java/com/example/splashscreen/below12home.java
@@ -21,7 +21,7 @@ public class below12home extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                Intent intent = new Intent(below12home.this, pg2below12.class);
+                Intent intent = new Intent(below12home.this, QuestionActivity.class);
                 startActivity(intent);
             }
         });
