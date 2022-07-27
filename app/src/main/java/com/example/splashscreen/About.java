12fc8a2ac1984d66d5bf5534e2fe9de0ae690038
jleenaife <107950772+jleenaife@users.noreply.github.com>
2022-07-27 19:13:03 +0800

    about page back button

--- app/src/main/java/com/example/splashscreen/About.java
+++ app/src/main/java/com/example/splashscreen/About.java
@@ -3,12 +3,24 @@ package com.example.splashscreen;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class About extends AppCompatActivity {
+
+    Button btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        btnBack =findViewById(R.id.btnAbtBack);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
     }
 }
