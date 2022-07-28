d78531e0ef69ef51a7aac178e88d43f2f072409b
avadecer <avadecer@gmail.com>
2022-07-28 23:47:47 +0800

    logo

--- app/src/main/java/com/example/splashscreen/GameView.java
+++ app/src/main/java/com/example/splashscreen/GameView.java
@@ -1,6 +1,7 @@
 package com.example.splashscreen;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
